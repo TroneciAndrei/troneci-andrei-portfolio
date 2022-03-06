f3dc61afef60e66e9875556cb3e74a16b0363d14
Troneci Andrei <troneciandrei@gmail.com>
2022-03-06 19:50:50 +0200

    Inceputul

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/input.css",
+    "./src/app.js",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'body':'#17171F',
+        'color': '#ff3636',
+      },
+    },
+  },
+  plugins: [],
+}
