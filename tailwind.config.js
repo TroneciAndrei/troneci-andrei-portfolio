1c861960f9a9367655d426e41f45ffd21d9adfd7
Troneci Andrei <troneciandrei@gmail.com>
2022-03-09 08:15:04 +0200

    Responsive changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-  content: [
-    "./src/index.html",
-    "./src/input.css",
-    "./src/app.js",
-  ],
+  content: ['index.html', './src/input.css', './src/app.js'],
   theme: {
     extend: {
-      colors:{
-        'body':'#17171F',
-        'color': '#ff3636',
+      colors: {
+        body: '#17171F',
+        color: '#ff3636',
+      },
+      screens: {
+        xs: '475',
       },
     },
   },
   plugins: [],
-}
+};
